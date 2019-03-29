6bb9e294457a96a73de6134a4e0204c4d6c30d2b
Eliza Khachatryan <elijbet@gmail.com>
2019-03-29 16:48:04 -0700

    Practicing props

--- 3-components/src/index.js
+++ 3-components/src/index.js
@@ -6,9 +6,9 @@ import CommentDetail from './CommentDetail';
 const App = () => {
 	return (
 		<div className="ui container comments">
-			<CommentDetail author="Jane" />
-			<CommentDetail author="Pete" />
-			<CommentDetail author="Kelly" />
+			<CommentDetail author="Jane" timeAgo="Yesterday at 3:34" image={faker.image.avatar()} text="Whoohoo learning React!" />
+			<CommentDetail author="Pete" timeAgo="Today at 5:34" image={faker.image.avatar()} text="Whoohoo learning Vue!"   />
+			<CommentDetail author="Kelly" timeAgo="Yesterday at 3:24" image={faker.image.avatar()} text="Whoohoo learning Angular!"   />
 		</div>
 	)
 }
